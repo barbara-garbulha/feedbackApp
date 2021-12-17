4172fb8ad7c8e464326ed95ea508b9c24ad8f305
barbara.garbulha <barbara.garbulha@gmail.com>
2021-12-17 09:31:29 -0500

    initial

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import Card from "../components/shared/Card";
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">About this project</div>
+      <p>This is a React app to leave feedback for a product or service</p>
+      <p>Version: 1.0.0</p>
+      <Link to="/">Back to Home</Link>
+    </Card>
+  );
+}
+
+export default AboutPage;
